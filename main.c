659b623bd0c91ed1840a38cfed3f37e458c0af34
succ3ss0r <m4no4shi@protonmail.ch>
2017-03-28 22:00:09 +0200

    interactions.c dodane do main i funkcja menu przeniesiona

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "structures.c"
+#include "interactions.c"
 
 int main()
 {
@@ -12,26 +13,5 @@ int main()
     return 0;
 }
 
-void menu(short opt)
-{
-    printf("Wybierz co chcesz zrobic:\n");
-    printf("\t1. Dodac klienta\n");
-    printf("\t2. Dodac dostawce\n");
-    printf("\t3. Usunac klienta\n");
-    printf("\t4. Usunac dostawce\n");
-    printf("\t5. Dodac produkt do magazynu\n");
-    printf("\t6. Usunac produkt z magazynu\n");
-    while(1)
-    {
-        scanf("%hi", &opt);
-        printf("Wybrales: %hi\n", opt);
-        if(opt>=1 && opt<=6){
-            puts("siema");
-            break;
-        }else{
-            printf("Cos poszlo nie tak! Wybierz ponownie co chcesz zrobic.\n");
-            continue;
-        }
-    }
-}
+
 
