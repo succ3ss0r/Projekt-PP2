4feec24caa2df65a87a4e470fa28548b2993a7ca
siwonpawel <siwonpawel@gmail.com>
2017-03-28 21:33:17 +0200

    DODANIE PLIKU interactions.c

--- main.c
+++ main.c
@@ -2,32 +2,18 @@
 #include <stdlib.h>
 
 #include "structures.c"
+#include "interactions.c"
 
 int main()
 {
     listsPointers listy;
     listInitialize(&listy);
-    int option;
+
+    int option = 0;
     menu(option);
 
     return 0;
 }
 
-void menu(int opt)
-{
-    printf("\t1. Dodac klienta\n");
-    printf("\t2. Dodac dostawce\n");
-    printf("\t3. Usunac klienta\n");
-    printf("\t4. Usunac dostawce\n");
-    while(1)
-    {
-        scanf("%d", &opt);
-        printf("Wybrales: %d", opt);
-        if(opt>=1 && opt<=4){
-            puts("siema");
-            break;
-        }else
-            continue;
-    }
-}
+
 
