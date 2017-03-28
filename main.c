552bcda5ed6511114a8a63d246505322720f1f97
succ3ss0r <m4no4shi@protonmail.ch>
2017-03-28 21:39:26 +0200

    funkcja menu dziala

--- main.c
+++ main.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "structures.c"
 
 int main()
 {
     listsPointers listy;
     listInitialize(&listy);
-    int option;
+    short option;
     menu(option);
 
     return 0;
 }
 
-void menu(int opt)
+void menu(short opt)
 {
+    printf("Wybierz co chcesz zrobic:\n");
     printf("\t1. Dodac klienta\n");
     printf("\t2. Dodac dostawce\n");
     printf("\t3. Usunac klienta\n");
     printf("\t4. Usunac dostawce\n");
+    printf("\t5. Dodac produkt do magazynu\n");
+    printf("\t6. Usunac produkt z magazynu\n");
     while(1)
     {
-        scanf("%d", &opt);
-        printf("Wybrales: %d", opt);
-        if(opt>=1 && opt<=4){
+        scanf("%hi", &opt);
+        printf("Wybrales: %hi\n", opt);
+        if(opt>=1 && opt<=6){
             puts("siema");
             break;
-        }else
+        }else{
+            printf("Cos poszlo nie tak! Wybierz ponownie co chcesz zrobic.\n");
             continue;
+        }
     }
 }
 
